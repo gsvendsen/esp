0f235613013e62950826dd62d7f114cba930dac6
Gustav Svendsen <gsvendsen99@gmail.com>
2019-11-15 02:00:09 +0100

    Added option to delete bookmark

--- src/pages/selectBookmark.js
+++ src/pages/selectBookmark.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, Dimensions } from 'react-native';
+import { Text, View, Image, Dimensions, TouchableOpacity } from 'react-native';
 var width = Dimensions.get('window').width; //full width
 import styled from 'styled-components'
 
@@ -14,7 +14,7 @@ export default SelectBookmark = (props) => {
     const Bookmark = styled.TouchableOpacity`
     margin:10px 0;
     flex:1;
-    padding:25px 0 25px 10px;
+    padding:20px 0 20px 10px;
     border-bottom-color: lightgrey;
     border-bottom-width: 1;
     `
@@ -26,6 +26,9 @@ export default SelectBookmark = (props) => {
 
     return (
         <View style={{flex:1, alignItems:"center"}}>
+          <TouchableOpacity onPress={() => props.onExit()} style={{width:30, height:30, position:"absolute", top:55, left:28}}>
+            <Image source={{uri: 'https://i.imgur.com/a7KJOYM.png'}} style={{width: 20, height: 20}} />
+          </TouchableOpacity>
           <Text style={{color:'black', fontWeight:'bold', fontSize:16, position:'absolute', top:55}}>Select Previous Flow</Text>
           <ScrollWrapper style={{width:width}}>
             <BookmarkContainer>
@@ -34,8 +37,13 @@ export default SelectBookmark = (props) => {
                   <Bookmark key={index} onPress={() => {
                     props.onBookmarkSelect(bookmark)
                   }}>
-                      <View style={{flex: 1}}>
+                      <View style={{flex: 1, position:"relative"}}>
                         <Text style={{color:"black", marginBottom:5}}>{bookmark.name}</Text>
+                        <TouchableOpacity onPress={() => {
+                          props.deleteBookmark(bookmark)
+                        }} style={{width:30, height:30, position:"absolute", right:20}}>
+                          <Image source={{uri: 'https://i.imgur.com/5IGnBuc.png'}} style={{width: 20, height: 20}} />
+                        </TouchableOpacity>
                       </View>
                   </Bookmark>
                 )
