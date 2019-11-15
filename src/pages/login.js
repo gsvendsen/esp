460466215dfff4cc4fdde2126986be1e24d19519
Gustav Svendsen <gsvendsen99@gmail.com>
2019-11-15 02:00:49 +0100

    Update frontpage text

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+var width = Dimensions.get('window').width; //full width
 
 
 export default Login = (props) => {
@@ -15,8 +17,8 @@ export default Login = (props) => {
               height: 300,
             }}
           />
-          <Image source={{uri: 'https://i.imgur.com/ijm0MzL.png'}} style={{width: 84, height: 123, marginBottom:35}} />
-          <Text style={{fontWeight:'bold', fontSize:18, marginBottom:100}}>Discover new music, epic slogan.</Text>
+          <Image source={{uri: 'https://i.imgur.com/ijm0MzL.png'}} style={{width: 84, height: 123, marginBottom:45}} />
+          <Text style={{fontWeight:'bold', fontSize:18, marginBottom:100}}>Discover new music in your way.</Text>
           <TouchableOpacity style={{height:69, width:width-30, backgroundColor:'#1db954'}} onPress={() => props.onLogin()}>
             <View style={{flex:1, flexDirection:"row", alignItems:"center", justifyContent:'center'}}>
               <Image source={{uri: 'https://i.imgur.com/H73GsK5.png'}} style={{width: 30, height: 30, marginRight:25}} />
